273fc6e66b4ab9b030824ee94705fb1bc6940524
Reese Lee <reese.lee.cy@gmail.com>
2019-04-12 11:29:58 -0700

    edit pizza constructor to get the  price, and add initial user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,40 @@
 // Business logic for pizza store
-function PizzaStore() {
-  this.pizzas = [];
-  this.currentPizza = -1
-}
-
-PizzaStore.prototype.addPizza = function(pizza) {
-  pizza.Id = this.assignId();
-  this.pizzas.push(push)
-}
-
-PizzaStore.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId
-}
-
-PizzaStore.prototype.findPizza = function(id) {
-  for(var i = 0, i < this.pizzas.length, i++) {
-    if (this.pizzas[i] {
-      if (this.pizzas[i].id == id) {
-        return this.pizzas[i];
-      }
-    }
-  };
-  return false;
-};
-
-PizzaStore.prototype.deletePizza = function(id) {
-  for(var i = 0, i < this.pizzas.length, i++) {
-    if (this.pizzas[i] {
-      if (this.pizzas[i].id == id) {
-        delete this.pizzas[i];
-      }
-    }
-  };
-  return false;
-};
+// function PizzaStore() {
+//   this.pizzas = [];
+//   this.currentPizza = -1
+// }
+//
+// PizzaStore.prototype.addPizza = function(pizza) {
+//   pizza.Id = this.assignId();
+//   this.pizzas.push(push)
+// }
+//
+// PizzaStore.prototype.assignId = function() {
+//   this.currentId += 1;
+//   return this.currentId
+// }
+//
+// PizzaStore.prototype.findPizza = function(id) {
+//   for(var i = 0, i < this.pizzas.length, i++) {
+//     if (this.pizzas[i] {
+//       if (this.pizzas[i].id == id) {
+//         return this.pizzas[i];
+//       }
+//     }
+//   };
+//   return false;
+// };
+//
+// PizzaStore.prototype.deletePizza = function(id) {
+//   for(var i = 0, i < this.pizzas.length, i++) {
+//     if (this.pizzas[i] {
+//       if (this.pizzas[i].id == id) {
+//         delete this.pizzas[i];
+//       }
+//     }
+//   };
+//   return false;
+// };
 
 // Business logic for Pizza
 function Pizza(sauce, toppings, crust, size) {
@@ -44,8 +44,9 @@ function Pizza(sauce, toppings, crust, size) {
   this.size = []
 }
 
-Pizza.prototype.sauceValue = function(sauce) {
-  var baseSauceValue = 1
+Pizza.prototype.getPrice = function() {
+  var sauceValue = function(sauce) {
+  var baseSauceValue = 1;
   if(this.sauce === 1) {
     baseToppingsValue +=1;
   } else if (this.sauce === 2) {
@@ -55,10 +56,10 @@ Pizza.prototype.sauceValue = function(sauce) {
   } else {
     alert("Please pick a scrumptious sauce for for that extra sugar coating!");
   }
-}
+// }
 
-Pizza.prototype.toppingsValue = function(topping) {
-  var baseToppingsValue = 1
+// Pizza.prototype.toppingsValue = function(topping) {
+  var baseToppingsValue = 1;
   if(this.toppings === 1) {
     baseToppingsValue +=1;
   } else if (this.toppings === 2) {
@@ -68,10 +69,10 @@ Pizza.prototype.toppingsValue = function(topping) {
   } else {
     alert("Please pick a delicious topping for maximum sugar rush!");
   }
-}
+// }
 
-Pizza.prototype.crustValue = function(crust) {
-  var baseCrustValue = 1
+// Pizza.prototype.crustValue = function(crust) {
+  var baseCrustValue = 1;
   if (this.crust === 1) {
     baseCrustValue += 1;
   } else if (this.crust === 2) {
@@ -79,26 +80,26 @@ Pizza.prototype.crustValue = function(crust) {
   } else if (this.crust === 3) {
     baseCrustValue += 3;
   } else {
-    alert("Are you crazy?! Choose a crust!");
+    alert("Are you crazy?! Choose a delicious crust!");
   }
-}
+// }
 
-Pizza.prototype.sizeValue = function(size) {
-  var baseSizeValue = 1
+// Pizza.prototype.sizeValue = function(size) {
+  var baseSizeValue = 1;
   if (this.size === 1) {
-    baseCrustValue += 1;
+    baseSizeValue += 1;
   } else if (this.size === 2) {
-    baseCrustValue += 2;
+    baseSizeValue += 2;
   } else if (this.size === 3) {
-    baseCrustValue += 3;
+    baseSizeValue += 3;
   } else {
-    alert("Eating for one? Eating for 5? We won't judge; just choose.");
+    alert("Eating for one? Eating for 5? We won't judge; just choose a size.");
   }
-}
+// }
 
-Pizza.prototype.getPrice = function() {
-  var pizzaPrice = 5
-  var baseOfAll = (baseSizeValue + baseCrustValue + baseSauceValue + baseToppingsValue)
+// Pizza.prototype.getPrice = function() {
+  var pizzaPrice = 5;
+  var baseOfAll = (baseSizeValue + baseCrustValue + baseSauceValue + baseToppingsValue);
   if(baseOfAll <= 4) {
     pizzaPrice +=4;
   } else if (baseOfAll >=5 && baseOfAll <=8) {
@@ -107,12 +108,25 @@ Pizza.prototype.getPrice = function() {
     pizzaPrice +=8;
   };
 };
+};
+
+
 
 
 
 // User interface logic
 $(function() {
-  var sizeInput =
-  var toppingsInput =
+  $("form#pizzaSelections").submit(function(event) {
+    event.preventDefault();
+    var sizeInput = $(parseInt("input:radio[name=size]:checked")).val();
+    var toppingsInput = $(parseInt("input:radio[name=toppings]:checked")).val();
+    var crustInput = $(parseInt("input:radio[name=crust]:checked")).val();
+    var sauceInput = $(parseInt("input:radio[name=sauce]:checked")).val();
+    var pizzaPrice = (sizeInput + toppingsInput + crustInput + sauceInput);
+    var pizzaMade = new Pizza(sauce, toppings, crust, size);
+    pizzaMade.getPrice();
+    console.log(pizzaPrice);
 
-})
+
+  });
+});
