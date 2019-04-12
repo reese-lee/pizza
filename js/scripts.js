bad51536a987a952b5ef40772e8c3780c5eedb47
Reese Lee <reese.lee.cy@gmail.com>
2019-04-12 15:34:33 -0700

    change if-else statements, push toppings into the toppings array, and make it all come together in one beautiful Pizza Object AND get the price!

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Business logic for Pizza
-function Pizza(toppings, size, price) {
+function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
 }
@@ -9,43 +9,43 @@ Pizza.prototype.addToppings = function(toppings) {
 }
 
 Pizza.prototype.addSize = function(size) {
-  this.size = sizeSelected;
+  this.size = size;
 }
 
 Pizza.prototype.getPrice = function(price) {
-  if (this.size === "Petite") {
-    if (this.toppings === "gummy" || this.toppings === "choc" || this.toppings === "charms") {
+  var toppings = [];
+  $.each($("input:checkbox[name=toppings]:checked"),
+  function() {
+    toppings.push($(this).val());
+  });
+  if (this.size === "Petite" && this.toppings.length ===1) {
     this.price = 8;
-    } else if ((this.toppings === "gummy" && this.toppings === "choc") || (this.toppings === "gummy" && this.toppings === "charms") || (this.toppings === "choc" && this.toppings ==="charms")) {
-      this.price = 8;
-      this.price +=4;
+    } else if (this.size === "Petite" && this.toppings.length ===2) {
+        this.price = 8;
+        this.price +=4;
+    } else if (this.size === "Petite" && this.toppings.length ===3) {
+        this.price = 8;
+        this.price +=8;
+    } else if (this.size === "In-Between" && this.toppings.length ===1) {
+        this.price = 10;
+        this.price +=4;
+    } else if (this.size === "In-Between" && this.toppings.length ===2) {
+        this.price = 10;
+        this.price +=6;
+    } else if (this.size === "In-Between" && this.toppings.length ===3) {
+        this.price = 10;
+        this.price +=8;
+    } else if (this.size === "Maximum" && this.toppings.length ===1) {
+        this.price = 12;
+    } else if (this.size === "Maximum" && this.toppings.length ===2) {
+        this.price = 12;
+        this.price +=8;
+    } else if (this.size === "Maximum" && this.toppings.length ===3) {
+        this.price = 12;
+        this.price +=10;
     } else {
-      this.price = 8;
-      this.price +=8;
+      console.log("y tho");
     }
-  return this.price;
-  if (this.size === "In-Between") {
-    if (this.toppings === "gummy" || this.toppings === "choc" || this.toppings === "charms")
-    this.price = 10;
-    } else if ((this.toppings === "gummy" && this.toppings === "choc") || (this.toppings === "gummy" && this.toppings === "charms") || (this.toppings === "choc" && this.toppings ==="charms")) {
-      this.price = 10
-      this.price +=4;
-    } else {
-      this.price = 10
-      this.price +=8;
-    }
-  return this.price;
-  if (this.size === "Maximum") {
-    if (this.toppings === "gummy" || this.toppings === "choc" || this.toppings === "charms")
-    this.price = 12;
-    } else if ((this.toppings === "gummy" && this.toppings === "choc") || (this.toppings === "gummy" && this.toppings === "charms") || (this.toppings === "choc" && this.toppings ==="charms")) {
-      this.price = 12
-      this.price +=4;
-    } else {
-      this.price = 12
-      this.price +=8;
-    }
-  };
   return this.price;
 };
 
@@ -53,17 +53,14 @@ Pizza.prototype.getPrice = function(price) {
 $(function() {
   $("form#pizzaSelections").submit(function(event) {
     event.preventDefault();
-    var sizeInput = $("input:radio[name=size]:checked").val();
-    var toppings = $.each($("input:checkbox[name=toppings]:checked"), function() {
+    var size = $("input:radio[name=sizeInput]:checked").val();
+    var toppings = [];
+    $.each($("input:checkbox[name=toppings]:checked"),
+    function() {
       toppings.push($(this).val());
     });
-    // var crustInput = $(parseInt("input:radio[name=crust]:checked")).val();
-    // var sauceInput = $(parseInt("input:radio[name=sauce]:checked")).val();
-    // var pizzaPrice = sizeInput + toppingsInput + crustInput + sauceInput;
-    // var pizzaMade = new Pizza(toppings, size, price);
-    // pizzaMade.getPrice();
-    // console.log(pizzaMade);
-    var pizzaMade = new Pizza(toppings, size, price);
+
+    var pizzaMade = new Pizza(toppings, size);
     var price = pizzaMade.getPrice();
     console.log(pizzaMade);
     console.log(price);
