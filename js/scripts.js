9f09cc4ad9d729a3cfec881c15cfdad9b97a561f
Reese Lee <reese.lee.cy@gmail.com>
2019-04-14 21:22:31 -0700

    condense the prototype for getPrice, add another submit button function, correct the code to push all selected toppings into an array, edit code to correctly calculate price based on size and number of toppings, delete console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,62 @@
 // Business logic for Pizza
-function Pizza(toppings, size) {
+function Pizza(toppings, size, crust, sauce) {
   this.toppings = toppings;
   this.size = size;
+  this.sauce = sauce;
+  this.crust = crust;
 }
 
-Pizza.prototype.getPrice = function(price) {
-  var toppings = [];
-  $.each($("input:checkbox[name=toppings]:checked"),
-  function() {
-    toppings.push($(this).val());
-  });
-  if (this.size === "Petite" && this.toppings.length ===1) {
-    this.price = 8;
-    } else if (this.size === "Petite" && this.toppings.length ===2) {
-        this.price = 8;
-        this.price +=4;
-    } else if (this.size === "Petite" && this.toppings.length ===3) {
-        this.price = 8;
-        this.price +=8;
-    } else if (this.size === "In-Between" && this.toppings.length ===1) {
-        this.price = 10;
-        this.price +=4;
-    } else if (this.size === "In-Between" && this.toppings.length ===2) {
-        this.price = 10;
-        this.price +=6;
-    } else if (this.size === "In-Between" && this.toppings.length ===3) {
-        this.price = 10;
-        this.price +=8;
-    } else if (this.size === "Maximum" && this.toppings.length ===1) {
-        this.price = 16;
-    } else if (this.size === "Maximum" && this.toppings.length ===2) {
-        this.price = 16;
-        this.price +=6;
-    } else if (this.size === "Maximum" && this.toppings.length ===3) {
-        this.price = 16;
-        this.price +=8;
+Pizza.prototype.getPrice = function() {
+  var price = 0;
+  if(this.size === "Petite") {
+    price += 8;
+  } else if (this.size === "In-Between") {
+    price += 12;
+  } else if (this.size === "Maximum") {
+    price += 16;
+  }
+  for (var i=0; i <= this.toppings.length -1; i++) {
+    if(this.toppings.length === 1) {
+      price +=2;
+    } else if (this.toppings.length === 2) {
+      price += 4;
+    } else if (this.toppings.length === 3) {
+      price += 6;
+    } else if (this.toppings.length === 4) {
+      price += 8;
+    } else if (this.toppings.length === 5) {
+      price += 10;
     } else {
-      alert("Please make your delicious selections! (Be sure to select both a size and at least one topping.)")
+      price += 12;
     }
-  return this.price;
+  return this.cost = price;
+  };
 };
 
-// User interface logic
 $(function() {
   $("form#pizzaSelections").submit(function(event) {
     event.preventDefault();
     var size = $("input:radio[name=sizeInput]:checked").val();
+    var sauce = $("input:radio[name=sauceBase]:checked").val();
+    var crust = $("input:radio[name=crustOptions]:checked").val();
     var toppings = [];
-    $.each($("input:checkbox[name=toppings]:checked"),
-    function() {
+    var pizzaMade = new Pizza(toppings, size, crust, sauce);
+    $("input:checkbox[name=toppings]:checked").each(function() {
       toppings.push($(this).val());
     });
-    var pizzaMade = new Pizza(toppings, size);
-    var price = pizzaMade.getPrice();
+    pizzaMade.getPrice();
+    var price = pizzaMade.cost;
     $(".results").show();
+    $("form#pizzaSelections").hide();
     $("#sizeSelected").text(pizzaMade.size);
-    $("#toppingsSelected").text(pizzaMade.toppings);
-    $("#priceOfSelected").text(pizzaMade.price);
+    $("#priceOfSelected").text(price);
+    $("#sauceSelected").text(sauce);
+    $("#crustSelected").text(crust);
+    $("#toppingsSelected").text(toppings);
+  });
+  $("#orderAgain").click(function(event) {
+    event.preventDefault();
+    $("form#pizzaSelections").show();
+    $(".results").hide();
   });
 });
